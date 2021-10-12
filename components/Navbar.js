bfeb386c8f6e9dd4a78ab2f95b4e2f06bd366aea
SANGROK KANG <sadreder@gmail.com>
2021-10-12 15:12:44 +0900

    testing CSS

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import Link from 'next/link';
+import '../styles/Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='logo'>
-        <h1>Logo Title</h1>
-      </div>
-      <Link href="/"><a>Home</a></Link>
-      <Link href="/guides/discover"><a>Discover</a></Link>
-      <Link href="/"><a>Docs</a></Link>
-      <Link href="/"><a>Distribute</a></Link>
-      <Link href="/"><a>Community</a></Link>
+    <nav className='menu'>
+      
+      <ul>
+        <li>
+          <a href='http://'>Home</a>
+        </li>
+        <li>
+          <a className='dropdown-arrow' href='http://'>
+            Products
+          </a>
+          <ul className='sub-menus'>
+            <li>
+              <a href='http://'>Products Sub Menu 1</a>
+            </li>
+            <li>
+              <a href='http://'>Products Sub Menu 2</a>
+            </li>
+            <li>
+              <a href='http://'>Products Sub Menu 3</a>
+            </li>
+            <li>
+              <a href='http://'>Products Sub Menu 4</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href='http://'>About</a>
+        </li>
+        <li>
+          <a className='dropdown-arrow' href='http://'>
+            Services
+          </a>
+          <ul className='sub-menus'>
+            <li>
+              <a href='http://'>Services Sub Menu 1</a>
+            </li>
+            <li>
+              <a href='http://'>Services Sub Menu 2</a>
+            </li>
+            <li>
+              <a href='http://'>Services Sub Menu 3</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href='http://'>Contact Us</a>
+        </li>
+      </ul>
     </nav>
   );
 };
